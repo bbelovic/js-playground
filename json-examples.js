cb7de5a1004d83fb04f6bf2bec6eb5fd1a2015c2
bbelovic <bbelovic@gmail.com>
2018-05-24 22:20:48 +0200

    Experiments with fetch api

--- json-examples.js
+++ json-examples.js
@@ -2,3 +2,8 @@ var json = '[{"name":"john", "surname":"doe"},{"name":"jane", "surname":"doe"}]'
 var people = JSON.parse(json);
 console.log(people.length);
 console.log(people[0].name)
+
+var itemsJson = '[{"name":"item 1","finished":true},{"name":"item2","finished":false}]'
+var items = JSON.parse(itemsJson)
+console.log(items[0].name);
+console.log(itemsJson[0].name)
